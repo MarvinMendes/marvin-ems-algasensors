09ea49f99d72694f0bbeb722ad26fff3bef4aa59
MarvinMendes <marvin.m.lopes@gmail.com>
2025-12-26 20:07:39 -0300

    Dockerizando o microserviço de Device-Management e adicionando as configurações de perfis.

--- algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/controller/SensorController.java
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/controller/SensorController.java
@@ -1,2 +1,27 @@
-package com.alga.algasensors.device.management.api.controller;public class SensorController {
+package com.alga.algasensors.device.management.api.controller;
+
+import com.alga.algasensors.device.management.api.model.SensorInput;
+import com.alga.algasensors.device.management.common.IdGenerator;
+import com.alga.algasensors.device.management.domain.model.Sensor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sensors")
+public class SensorController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Sensor create(@RequestBody SensorInput input) {
+        return Sensor.builder()
+                .id(IdGenerator.generateTSID())
+                .name(input.getName())
+                .ip(input.getIp())
+                .location(input.getLocation())
+                .protocol(input.getProtocol())
+                .model(input.getModel())
+                .enable(false)
+                .build();
+    }
+
 }
--- algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/domain/model/Sensor.java
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/domain/model/Sensor.java
@@ -1,2 +1,22 @@
-package com.alga.algasensors.device.management.domain.model;public class Sensor {
+package com.alga.algasensors.device.management.domain.model;
+
+import io.hypersistence.tsid.TSID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Sensor {
+
+    private TSID id;
+    private String name;
+    private String ip;
+    private String location;
+    private String protocol;
+    private String model;
+    private Boolean enable;
 }

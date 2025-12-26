ae4554502ac2d410d94993c11af0778065bfe01e
MarvinMendes <marvin.m.lopes@gmail.com>
2025-12-26 20:07:39 -0300

    Dockerizando o microserviço de Device-Management e adicionando as configurações de perfis.

--- algasensors-meta/microservices/algasensors-device-management/device-management/src/test/java/com/alga/algasensors/device/management/TSIDTest.java
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/test/java/com/alga/algasensors/device/management/TSIDTest.java
@@ -1,2 +1,17 @@
-package com.alga.algasensors.device.management;public class TSIDTest {
+package com.alga.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.junit.jupiter.api.Test;
+
+class TSIDTest {
+
+    @Test
+    void shouldGenerateTSID() {
+        TSID tsid = TSID.Factory.getTsid();
+
+        System.out.println(tsid);
+        System.out.println(tsid.toLong());
+    }
+
+
 }

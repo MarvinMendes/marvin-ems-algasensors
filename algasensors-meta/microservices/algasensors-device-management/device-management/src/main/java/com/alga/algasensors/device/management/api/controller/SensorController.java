015465ab1356a1f3e6d9f7e32e663f652955670c
MarvinMendes <marvin.m.lopes@gmail.com>
2025-12-27 10:09:02 -0300

    Busca por id e busca paginada de registros de sensor

--- algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/controller/SensorController.java
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/controller/SensorController.java
@@ -6,9 +6,17 @@ import com.alga.algasensors.device.management.common.IdGenerator;
 import com.alga.algasensors.device.management.domain.model.Sensor;
 import com.alga.algasensors.device.management.domain.model.SensorId;
 import com.alga.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.NotFound;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +38,10 @@ public class SensorController {
                 .enable(false)
                 .build();
         repository.saveAndFlush(sensor);
+        return convert(sensor);
+    }
+
+    private SensorOutput convert(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getSensorId())
                 .name(sensor.getName())
@@ -40,5 +52,21 @@ public class SensorController {
                 .enable(sensor.getEnable())
                 .build();
     }
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convert(sensor);
+    }
+
+    @GetMapping
+    public Page<SensorOutput> search(@PageableDefault Pageable pageable) {
+        Page<Sensor> sensorPage = repository.findAll(pageable);
+
+        return sensorPage.map(this::convert);
+
+    }
+
+
 
 }

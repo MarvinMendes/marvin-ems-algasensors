a4f7ea5473f84b88eb6a786032a6d2492273959f
MarvinMendes <marvin.m.lopes@gmail.com>
2025-12-28 16:07:11 -0300

    Busca de detalhes da temperature via Rest Client

---
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/model/SensorDetailOutput.java
@@ -0,0 +1,2 @@
+package com.alga.algasensors.device.management.api.model;public class SensorDetailOutput {
+}
---
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/model/SensorMonitoringOutput.java
@@ -0,0 +1,19 @@
+package com.alga.algasensors.temperature.monitoring.api.model;
+
+import com.alga.algasensors.temperature.monitoring.domain.model.SensorId;
+import io.hypersistence.tsid.TSID;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+public class SensorMonitoringOutput {
+
+    private TSID id;
+    private Double lastTemperature;
+    private OffsetDateTime updateAt;
+    private Boolean enabled;
+
+}

b779c50f08b457a55e7ca398e7a0a6a29487fefd
MarvinMendes <marvin.m.lopes@gmail.com>
2025-12-29 19:50:07 -0300

    Configurando a serialização para envio de mensagens com o Jackson. | Criação da publicação de mensagens

--- algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/model/SensorDetailOutput.java
+++ algasensors-meta/microservices/algasensors-device-management/device-management/src/main/java/com/alga/algasensors/device/management/api/model/SensorDetailOutput.java
@@ -1,2 +1,13 @@
-package com.alga.algasensors.device.management.api.model;public class SensorDetailOutput {
+package com.alga.algasensors.device.management.api.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SensorDetailOutput {
+
+    private SensorOutput sensor;
+    private SensorMonitoringOutput monitoring;
+
 }
---
+++ algasensors-meta/microservices/algasensors-temperature-processing/temperature-processing/temperature-processing/src/main/java/com/alga/algasensors/temperature/processing/controller/TemperatureProcessingController.java
@@ -0,0 +1,59 @@
+package com.alga.algasensors.temperature.processing.controller;
+
+import com.alga.algasensors.temperature.processing.common.IdGenerator;
+import com.alga.algasensors.temperature.processing.model.TemeperatureLogOutput;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.awt.*;
+import java.time.OffsetDateTime;
+
+import static com.alga.algasensors.temperature.processing.infraestructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/sensors/{sensorId}/temperatures/data")
+@RequiredArgsConstructor
+public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+        if(input == null || input.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Double temperature;
+
+        try {
+            temperature = Double.parseDouble(input);
+        } catch (NumberFormatException e)  {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        TemeperatureLogOutput logOutput = TemeperatureLogOutput.builder()
+                .id(IdGenerator.generateTimeBasedUUID())
+                .sensorId(sensorId)
+                .value(temperature)
+                .registeredAt(OffsetDateTime.now())
+                .build();
+
+        String exchange = FANOUT_EXCHANGE_NAME;
+        String routingKey = "";
+        TemeperatureLogOutput payload = logOutput;
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+
+        log.info(logOutput.toString());
+
+    }
+
+
+}
